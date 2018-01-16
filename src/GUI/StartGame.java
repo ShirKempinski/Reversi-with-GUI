b4f7999df5b8cd3dee90b86f27b2537078073c16
1 <shoham200296@gmail.com>
2018-01-16 03:46:51 +0200

    doing the StatGame class

--- src/GUI/StartGame.java
+++ src/GUI/StartGame.java
@@ -1,23 +1,29 @@
 package GUI;
 
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-public class StartGame {
+public class StartGame extends Application {
 
     public void start(Stage primaryStage) {
         try {
-            GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("Menu.fxml"));
+            GridPane root = (GridPane) FXMLLoader.load(getClass().getResource("StartGame.fxml"));
             Scene scene = new Scene(root, 600, 450);
-            scene.getStylesheets().add(getClass().getResource("Menu.css").toExternalForm());
-            primaryStage.setTitle("Reversi Game");
+            scene.getStylesheets().add(getClass().getResource("StartGame.css").toExternalForm());
+            primaryStage.setTitle("Reversi");
             primaryStage.setScene(scene);
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
 
