0ae38f1c22303857b7180e5cc4e7bc127b426d7d
shohamHadad <33810472+shohamHadad@users.noreply.github.com>
2018-01-18 13:46:56 +0200

    Update SquareListener.java

--- src/GUI/SquareListener.java
+++ src/GUI/SquareListener.java
@@ -6,14 +6,34 @@ public class SquareListener {
 
 	private ReversiController rc;
 	
+	
+	/**
+	 * function name: SquareListener
+	 * input: ReversiController controller
+	 * output: new object of SquareListener
+	 * operation: constructor
+	 */
 	public SquareListener(ReversiController controller) {
 		this.rc = controller;
 	}
 	
+	
+	/**
+	 * function name: clickEvent
+	 * input: Square s
+	 * output: void
+	 * operation: The function runs the game again after the player selects his or her move.
+	 */
 	public void clickEvent(Square s) {
 		rc.playOneTurn(s);
 	}
 	
+	/**
+	 * function name: hoverEvent
+	 * input: GUISquare s
+	 * output: void
+	 * operation: The function hover hevnt.
+	 */
 	public void hoverEvent(GUISquare s) {
 		s.setHover();
 	}
